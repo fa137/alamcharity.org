1c03fed76edb01954db0a29d436321d0174bbfeb
Fahimnur Alam <fahim@codexi.com>
2023-03-26 22:34:22 -0400

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -1,14 +1,14 @@
 import defaultImage from "./assets/images/alamcharity-orphanage.jpg";
 
 const CONFIG = {
-	name: "AlamCharity",
+	name: "alamcharity",
 
 	origin: "https://alamcharity.org",
 	basePathname: "/",
 	trailingSlash: false,
 
-	title: "alamcharity.org",
-	description: "towards a better future",
+	title: "alamcharity",
+	description: "A nonprofit organization dedicated to providing support and assistance to orphanages and underprivileged communities.",
 	defaultImage: defaultImage,
 
 	defaultTheme: "system", // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
